4d9eafba14d7d525592210a0ddd836df5a2d5ddc
Argenis Dominguez <argenisdominguez@Argeniss-MacBook-Pro.local>
2020-09-23 13:29:31 -0400

    Modified app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark></v-app-bar>
+    <v-app-bar app color="primary" dark dense></v-app-bar>
     <v-main></v-main>
   </v-app>
 </template>
